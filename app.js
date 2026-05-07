1a729f3084a7ee854e109fb669e3720c48abe9e4
Luc <zgbongue@gmail.com>
2026-05-07 18:02:16 +0000

    creation de fonction pour le bouton ajouter

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // Selection des elements html//
 let input = document.querySelector(".input-tache");
+let bouton = document.querySelector(".btn-ajout");
+let liste = document.querySelector(".tache-list");
+
+// Fonction pour ajout de tache
+function ajouterTache(){
+    let text = input.value.trim();
+
+    // reinitialiser le champ de saisie apres une saisie 
+    input.value = "";
+
+    // Condition: si aucune tache n'est saisie, alerter au click!!! et revenir
+    if (text === "") {
+        alert("Entrez une tâche!");
+        return;
+    }
+
+    // Creation de l'element li pour génerer les listes
+    let li = document.createElement("li");
+    li.textContent = text;
+
+    // ajout du li dans la liste ou la balise ul 
+    liste.appendChild(li);
+
+    // // creation du bouton pour editer les taches
+    // let editButton = document.createElement("")
+}
